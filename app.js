b340440fa6cab091433660ffe24178cd90606d6d
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-02-14 23:56:37 +0000

    Express training - Routing and Object params

--- app.js
+++ app.js
@@ -26,7 +26,45 @@ app.listen(3000, "127.0.0.1", () => {
 // console.log(`Welcome ${name} sit down and relax`);
 // });
 
-// req.get and referrer
-app.all("/", req => {
-  console.log(req.get("Referer"));
+//  ------ EX5. req.get and Referrer/Referer ------
+// app.all("/", req => {
+//   console.log(req.get("Referer"));
+// });
+
+//  ------ EX6. Routing and Object params ------
+//first path
+// app.get("/hello/new-user", req => {
+//   console.log("Adding new user");
+// });
+// //second path (order does matter)
+// app.get("/hello/:name", req => {
+//   console.log("Welcoming visitor");
+// });
+
+app.get("/hello/:name", req => {
+  console.log(`Hi ${req.params.name}`);
+});
+
+app.get("/article/:id/:title?", req => {
+  console.log(req.params);
+});
+
+// app.all("/", req => {
+//   console.log("Guest list");
+// });
+
+app.get("/:id", req => {
+  console.log(`Guest with id${req.params.id} detail info`);
+});
+
+// app.post("/", req => {
+//   console.log("Adding new guest");
+// });
+
+app.patch("/:id", req => {
+  console.log(`Guest with id${req.params.id} update`);
+});
+
+app.delete("/:id", req => {
+  console.log("Deleting guest with id${req.params.id}");
 });
